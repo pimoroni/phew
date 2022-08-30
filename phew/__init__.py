a21bc19fe817840589f63246d9bf3e31f378d7e1
lowfatcode <jon@magicnumber.co.uk>
2022-08-30 05:10:42 +0100

    add access point helper and remote mount detect

--- phew/__init__.py
+++ phew/__init__.py
@@ -3,18 +3,26 @@ __version__ = "0.0.2"
 # highly recommended to set a lowish garbage collection threshold
 # to minimise memory fragmentation as we sometimes want to
 # allocate relatively large blocks of ram.
-import gc
+import gc, os
 gc.threshold(50000)
 
 # phew! the Pico (or Python) HTTP Endpoint Wrangler
 from . import logging
 
+# determine if remotely mounted or not, changes some behaviours like
+# logging truncation
+remote_mount = False
+try:
+  os.statvfs(".") # causes exception if remotely mounted (mpremote/pyboard.py)
+except:
+  logging.debug("> detected remotely mounted filesystem")
+  remote_mount = True
+
 # helper method to quickly get connected to wifi
 def connect_to_wifi(ssid, password, timeout_seconds=30):
   import network, time
 
-  logging.debug("> connecting to wifi network:", ssid)
-
+  rp2.country("GB")
   wlan = network.WLAN(network.STA_IF)
   wlan.active(True)
   wlan.connect(ssid, password)
@@ -26,9 +34,26 @@ def connect_to_wifi(ssid, password, timeout_seconds=30):
     time.sleep(0.1)
 
   if wlan.status() != 3:
-    logging.debug("  - failed to connect")
     return None
 
-  logging.debug("  - ip address: ", wlan.ifconfig()[0])
-
   return wlan.ifconfig()[0]
+
+# helper method to put the pico into access point mode
+def access_point(ssid, password = None):
+  import rp2, network
+
+  # start up network in access point mode
+  rp2.country("GB")
+  ap = network.WLAN(network.AP_IF)
+  if ap.isconnected():
+    ap.disconnect()
+  ap.active(False)
+
+  ap.config(essid=ssid)
+  if password:
+    ap.config(password=password)
+  else:    
+    ap.config(security=0) # disable password
+  ap.active(True)
+
+  return ap
--- phew/logging.py
+++ phew/logging.py
@@ -1,64 +1,74 @@
 import machine, os, time, network, gc
 
+log_file = "log.txt"
+
+# the log file will be truncated if it exceeds _log_truncate_at bytes in
+# size. the defaults values are designed to limit the log to at most
+# three blocks on the Pico
+_log_truncate_at = 11 * 1024
+_log_truncate_to =  8 * 1024
+
+
+
 def datetime_string():
   dt = machine.RTC().datetime()
   return "{0:04d}-{1:02d}-{2:02d} {4:02d}:{5:02d}:{6:02d}".format(*dt)
 
-_log_file = "log.txt"
-def log_file(file):
-  global _log_file
-  _log_file = file
+
+def file_size(file):
+  try:
+    return os.stat(file)[6]
+  except OSError:
+    return None
 
 # truncates the log file down to a target size while maintaining
 # clean line breaks
-def truncate(target):
+def truncate(file, target_size):
   # get the current size of the log file
-  size = 0
-  try:
-    size = os.stat(_log_file)[6]
-  except OSError:
-    return
-  # calculate how many bytes we're aiming to discard
-  discard = size - target
+  size = file_size(file)
 
+  # calculate how many bytes we're aiming to discard
+  discard = size - target_size
   if discard <= 0:
     return
 
-  with open(_log_file, "rb") as infile:
-    # skip a bunch of the input file until we've discarded
-    # at least enough
-    while discard > 0:
-      chunk = infile.read(1024)
-      discard -= len(chunk)
+  with open(file, "rb") as infile:
+    with open(file + ".tmp", "wb") as outfile:
+      # skip a bunch of the input file until we've discarded
+      # at least enough
+      while discard > 0:
+        chunk = infile.read(1024)
+        discard -= len(chunk)
 
-    with open(_log_file + ".tmp", "wb") as outfile:
-      # we have a partial chunk to write, try to find a line
-      # break nearby to split it on
-      break_position = chunk.find(b"\n", -discard)
-      if break_position == -1:
-        break_position = chunk.rfind(b"\n", -discard)
-      if break_position == -1:
-        break_position = 0
-      outfile.write(chunk[break_position + 1:])
+      # try to find a line break nearby to split first chunk on
+      break_position = max(
+        chunk.find (b"\n", -discard), # search forward
+        chunk.rfind(b"\n", -discard) # search backwards
+      )
+      if break_position != -1: # if we found a line break..
+        outfile.write(chunk[break_position + 1:])
 
       # now copy the rest of the file
       while True:
         chunk = infile.read(1024)
-        if not chunk:
+        if not chunk: 
           break
         outfile.write(chunk)
 
   # delete the old file and replace with the new
-  os.remove(_log_file)
-  os.rename(_log_file + ".tmp", _log_file)
+  os.remove(file)
+  os.rename(file + ".tmp", file)
 
 
 def log(level, text):
   datetime = datetime_string()
   log_entry = "{0} [{1:8} /{2:>8}] {3}".format(datetime, level, gc.mem_free(), text)
   print(log_entry)
-  with open(_log_file, "a") as logfile:
+  with open(log_file, "a") as logfile:
     logfile.write(log_entry + '\n')
+
+  if _log_truncate_at and file_size(log_file) > _log_truncate_at:
+    truncate(log_file, _log_truncate_to)
 
 def info(*items):
   log("info", " ".join(map(str, items)))
