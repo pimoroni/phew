418fb603c302b45108d5d5b3db4c11cb4bdd62ba
lowfatcode <jon@magicnumber.co.uk>
2022-09-02 11:39:15 +0100

    removed excess whitespace

--- phew/logging.py
+++ phew/logging.py
@@ -8,12 +8,9 @@ log_file = "log.txt"
 _log_truncate_at = 11 * 1024
 _log_truncate_to =  8 * 1024
 
-
-
 def datetime_string():
   dt = machine.RTC().datetime()
   return "{0:04d}-{1:02d}-{2:02d} {4:02d}:{5:02d}:{6:02d}".format(*dt)
-
 
 def file_size(file):
   try:
